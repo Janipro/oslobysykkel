866520f9c2438a1680392e6efd5412cf43136e4c
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2020-11-07 23:16:07 +0100

    Typo

--- src/3largest.py
+++ src/3largest.py
@@ -10,22 +10,28 @@ def highest_product_sum(integer_list):
     temp = list(integer_list)
     result = 1
     negatives = 0
+    choices = []
 
-    for number in temp:
-        if number < 0:
-            negatives += 1
-
-    if len(temp) < 3 or negatives % 2 != 0:
-        return -1
+    if len(temp) < 3:
+        return
 
     for i in range(3):
         max_element = max(temp)
         min_element = min(temp)
         if abs(max_element) < abs(min_element):
             result *= min(temp)
+            choices.append(min(temp))
             temp.remove(min(temp))
         else:
             result *= max(temp)
+            choices.append(max(temp))
             temp.remove(max(temp))
+
+    for number in choices:
+        if number < 0:
+            negatives += 1
+
+    if negatives % 2 != 0:
+        return
 
     return result
