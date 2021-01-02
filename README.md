a3329e8d5a468ed2fadf3c442772d6f7963e5155
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2021-01-02 12:58:04 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Oslo city bike project
 Application utilizing the Oslo city bike API to view stations and if there are any available bikes. <br/>
+
+## Installation
+This project uses two modules to complete its task. <br>
+These can easily be installed using **pip**. <br>
+
+### Pip
+To make sure you have pip installed, do the following: <br> 
+* Open command prompt
+* Run `pip -V`
+* The result should look something like this: `pip 20.3.3 from c:\users\INSERT_PATH`
+
+If you **do not have** pip, 
+the latest version can be found [here](https://pypi.org/project/pip/#files) <br>
+If you do have pip, but need to **update**, simply run `py -m pip install -U pip`
+
+### Modules
+If you already have pip installed, open command prompt and do the following: <br>
+
+```shell script
+foo@bar> pip install requests
+foo@bar> pip install PySimpleGUI
+```
+
+## Run
+Running the program is **easy**. <br>
+Open command prompt and `cd` to `oslobysykkel/src`. <br>
+Then simply do: `py oslobysykkel.py`.
--- src/oslobysykkel.py
+++ src/oslobysykkel.py
@@ -13,24 +13,30 @@ if not requests:
 if not sg:
     print("Module PySimpleGUI has not been imported correctly!")
 
-info = {
-    "Client-Identifier": "privat-intervju"
+
+def get_data():
+    info = {
+    "Client-Identifier": "privat_bruk"
 }
+    response_stations = requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json", params=info)
+    response_availability = requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json", params=info)
+    stations_data = response_stations.json()["data"]["stations"]
+    availability_data = response_availability.json()["data"]["stations"]
+    data = {}
 
-response_stations = requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json", params=info)
-response_availability = requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json", params=info)
-stations_data = response_stations.json()["data"]["stations"]
-availability_data = response_availability.json()["data"]["stations"]
-data = {}
+    for i in range(len(stations_data)):
+        """Using range because len(stations_data) == len(availability_data)"""
+        station = stations_data[i]["name"]
+        position = (stations_data[i]["lat"], stations_data[i]["lon"])
+        available_bikes = availability_data[i]["num_bikes_available"]
+        free_spots = availability_data[i]["num_docks_available"]
+        data[station] = (available_bikes, free_spots, position)
 
-for i in range(len(stations_data)):
-    """Using range because len(stations_data) == len(availability_data)"""
-    station = stations_data[i]["name"]
-    available_bikes = availability_data[i]["num_bikes_available"]
-    free_spots = availability_data[i]["num_docks_available"]
-    data[station] = (available_bikes, free_spots)
+    return data
 
-stations = list(data.keys())
+
+current_data = get_data()
+stations = list(current_data.keys())
 stations.sort()
 
 stations_list_column = [
@@ -59,23 +65,29 @@ layout = [
     ],
 ]
 
-window = sg.Window("Oslo Bysykkel", layout, size=(800, 600))
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        raise SystemExit
 
-    if event == "-SEARCH-":
-        search = values["-SEARCH-"]
-        temp = []
-        for station in stations:
-            if search in station or search in station.lower():
-                temp.append(station)
-        window["-STATIONS-"].update(temp)
+def main():
+    window = sg.Window("Oslo Bysykkel", layout, size=(800, 600))
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            raise SystemExit
 
-    if event == "-STATIONS-":
-        name = values["-STATIONS-"][0]
-        number_of_bikes = data.get(name)[0]
-        number_of_spots = data.get(name)[1]
-        window["-BIKES-"].update(number_of_bikes)
-        window["-SPOTS-"].update(number_of_spots)
+        if event == "-SEARCH-":
+            search = values["-SEARCH-"]
+            temp = []
+            for station in stations:
+                if search in station or search in station.lower():
+                    temp.append(station)
+            window["-STATIONS-"].update(temp)
+
+        if event == "-STATIONS-":
+            name = values["-STATIONS-"][0]
+            number_of_bikes = current_data.get(name)[0]
+            number_of_spots = current_data.get(name)[1]
+            window["-BIKES-"].update(number_of_bikes)
+            window["-SPOTS-"].update(number_of_spots)
+
+
+main()
+
